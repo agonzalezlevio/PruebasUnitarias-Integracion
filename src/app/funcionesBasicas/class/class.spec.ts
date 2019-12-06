83d660cd96150f39e04123aabb07d377440cfa24
axlfrost <ale.gonzalezlevio@gmail.com>
2019-12-06 00:09:21 -0300

    Actualización pruebas de clases

--- src/app/funcionesBasicas/class/class.spec.ts
+++ src/app/funcionesBasicas/class/class.spec.ts
@@ -3,11 +3,46 @@ import { Jugador } from './class';
 
 describe('Pruebas de clase', () => {
 
+    // Jugador de prueba
+    let jugador = new Jugador();
+  /*
+    Ciclos de vida
+
+    // Inicio
+    beforeAll(); // Global
+    beforeEach();   // Especifica
+
+    // Finalización
+    afterAll(); // Global
+    afterEach(); // Especifica
+
+ */
+
+    beforeAll(() => {
+        console.log('beforeAll');
+    });
+
+    beforeEach(() => {
+        console.log('beforeEach');
+        // Reiniciar el jugador
+        jugador = new Jugador();
+    });
+
+    afterAll(() => {
+        console.log('afterAll');
+    });
+
+    afterEach(() => {
+        console.log('afterEach');
+    });
+
+
+
+
+
 
     it('Debe de retornar 80 de vida, si recibe 20 de daño', () => {
 
-        // Jugador de prueba
-        const jugador = new Jugador();
         const resp = jugador.recibeDamage(20);
 
         expect(resp).toBe(80);
@@ -17,8 +52,6 @@ describe('Pruebas de clase', () => {
 
     it('Debe de retornar 50 de vida, si recibe 50 de daño', () => {
 
-        // Jugador de prueba
-        const jugador = new Jugador();
         const resp = jugador.recibeDamage(50);
 
         expect(resp).toBe(50);
